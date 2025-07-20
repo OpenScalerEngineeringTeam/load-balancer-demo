d0da1de0d4ab7dd4a12103eaec59851599e46f40
OpenScalerEngineeringTeam <openscaler@gmail.com>
2025-07-20 13:38:59 +0100

    Fixed bugs

--- src/install.sh
+++ src/install.sh
@@ -2,10 +2,9 @@
 # -------------------------------------------------------------------------------------------------
 #   The following script works assuming "psutil" and "flask" are installed.
 # -------------------------------------------------------------------------------------------------
-
-mv load-balancer-demo.service /etc/systemd/system/load-balancer-demo.service
+cp /root/load-balancer-demo/src/load-balancer-demo.service /etc/systemd/system/load-balancer-demo.service
 chmod 644 /etc/systemd/system/load-balancer-demo.service
-chmod +x /root/load-balancer-demo/load-balancer-demo.py
+chmod +x /root/load-balancer-demo/src/load-balancer-demo.py
 
 systemctl daemon-reexec
 systemctl daemon-reload
