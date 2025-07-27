413e1274320aaf1b5f510b7eefa06fc86fc49cb4
OpenScalerEngineeringTeam <openscaler@gmail.com>
2025-07-27 14:13:12 +0100

    Fixed issues in installation scripts

--- admin_app/scripts/debian.sh
+++ admin_app/scripts/debian.sh
@@ -16,11 +16,9 @@ packages:
 runcmd:
 # download source release
 - cd /root
-- wget https://github.com/OpenScalerEngineeringTeam/load-balancer-demo/archive/refs/tags/v0.0.4.tar.gz -O /root/load-balancer-demo.tar.gz
+- wget https://github.com/OpenScalerEngineeringTeam/load-balancer-demo/archive/refs/tags/v0.0.5.tar.gz -O /root/load-balancer-demo.tar.gz
 - tar -xzf /root/load-balancer-demo.tar.gz -C /root/
 - rm /root/load-balancer-demo.tar.gz
-- mv /root/load-balancer-demo-0.0.4 /root/load-balancer-demo
-# Install dependencies
-- cd /root/load-balancer-demo/admin_app
-- source scripts/utils.sh
-- source scripts/install-dependencies.sh
+- mv /root/load-balancer-demo-0.0.5 /root/load-balancer-demo
+# Install dependenciess
+- cd /root/load-balancer-demo/admin_app && . scripts/utils.sh && . scripts/install-dependencies.sh
